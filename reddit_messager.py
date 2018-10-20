4c3f866cc47812da62da45afcae3cce9952e2d57
J.L <37510055+Johk3@users.noreply.github.com>
2018-10-20 15:00:29 +0300

    Update reddit_messager.py

--- reddit_messager.py
+++ reddit_messager.py
@@ -36,20 +36,20 @@ while True:
     except KeyboardInterrupt:
         key = raw_input("Theboysxx or Blazejohk? Or just exit?\n")
         if key.lower() == "theboysxx":
-            reddit = praw.Reddit(client_id='WwIsoVyRgqOKmQ',
-                                 client_secret='tgGQkIIS6Oa8pkJP9WBKbqbXqFI',
-                                 password='6bEZkox56bvaXauS36nbJd14HnpzD',
+            reddit = praw.Reddit(client_id='id',
+                                 client_secret='secret',
+                                 password='password',
                                  user_agent='testscript by /u/fakebot3',
-                                 username='theboysxx')
+                                 username='username')
             user = "Theboysxx"
         elif key.lower() == "exit":
             exit(0)
         else:
-            reddit = praw.Reddit(client_id='NW8OLOjmojgpag',
-                                 client_secret='vlNBaN-UMFucIeZWM6rLRAAq_40',
-                                 password='johannes2003',
+            reddit = praw.Reddit(client_id='id',
+                                 client_secret='secret',
+                                 password='password',
                                  user_agent='testscript by /u/fakebot3',
-                                 username='blazejohk')
+                                 username='username')
             user = "Blazejohk"
 
         system("cls")
